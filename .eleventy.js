342ed873bfc3723a95a3eb0fef233fac6eefdbd3
kk <post@koljakutschera.de>
2018-11-21 19:51:50 +0100

    documentation...

--- .eleventy.js
+++ .eleventy.js
@@ -1,20 +1,18 @@
 const { DateTime } = require('luxon');
 
 module.exports = function (eleventyConfig) {
-  // Add a filter using the Config API
+  // Do not delete we need this in: site/_data/permalink.js
   eleventyConfig.addFilter("replaceEmptyWithIndex", function(value) {
     return !value || value === "" ? "index" : value;
   });
-
+  // The following filters are just examples for the simple-blog-example
   eleventyConfig.addFilter('readableDate', (dateObj) => {
     return DateTime.fromJSDate(dateObj).toFormat('dd LLLL yyyy');
   });
-
   // https://html.spec.whatwg.org/multipage/common-microsyntaxes.html#valid-date-string
   eleventyConfig.addFilter('htmlDateString', (dateObj) => {
     return DateTime.fromJSDate(dateObj).toFormat('yyyy-LL-dd');
   });
-
   // You can return your Config object (optional).
   return eleventyConfig;
 };
--- site/_data/permalink.js
+++ site/_data/permalink.js
@@ -1,1 +1,2 @@
+// Because parcel does not(now) support eleventys folder-urls we auto-flatten the file-structure using parmalink
 module.exports = "/{{ page.fileSlug | replaceEmptyWithIndex | slug }}.html";
