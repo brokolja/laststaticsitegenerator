ff9f88ad1ee7df22c6648b76dced475a328c31cd
Kolja Kutschera <post@koljakutschera.de>
2019-04-10 18:18:50 +0200

    fix: remove unused example eleventy-filters

--- .eleventy.js
+++ .eleventy.js
@@ -1,19 +1,7 @@
-const { DateTime } = require("luxon");
-
 module.exports = function(eleventyConfig) {
   eleventyConfig.setDataDeepMerge(true);
   eleventyConfig.addFilter("urlFix", function(value) {
     value = !value || value === "" || value === "/" ? "index" : value;
     return value.indexOf(".html") < 0 ? value + ".html" : value;
-  });
-  eleventyConfig.addFilter("htmlDateString", dateObj => {
-    return DateTime.fromJSDate(dateObj).toFormat("yyyy-LL-dd");
-  });
-  eleventyConfig.addFilter("readableDate", dateObj => {
-    return DateTime.fromJSDate(dateObj).toFormat("dd LLLL yyyy");
-  });
-  eleventyConfig.addFilter("head", (array, n) => {
-    if (n < 0) return array.slice(n);
-    return array.slice(0, n);
   });
 };
